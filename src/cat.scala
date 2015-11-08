ba9290abf1a0341c843df5d4ce4b61cb424c025d
Nader Hadji Ghanbari <nader.h.ghanbari@gmail.com>
2015-11-08 19:36:04 +0800

    Improved `cat` by using a more functional version (writing some reusable functions).

--- src/cat.scala
+++ src/cat.scala
@@ -2,6 +2,14 @@
 
 /** Concatenates and prints file contents. */
 
-def files = args map (new java.io.File(_)) filter (_.exists) filter (_.isFile)
-def lines = files map io.Source.fromFile map (_.getLines) flatMap (_.toStream)
-lines foreach println
+def asFileStream(fileNames: Array[String]) = fileNames.toStream map (new java.io.File(_))
+
+def existingFiles(candidates: Stream[java.io.File]) = candidates filter (_.isFile)
+
+def lineStream(file: java.io.File) = io.Source.fromFile(file).getLines().toStream
+
+lazy val lines = existingFiles(asFileStream(args)) flatMap lineStream
+
+lines foreach {
+  println
+}
