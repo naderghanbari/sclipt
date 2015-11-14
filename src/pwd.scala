ef96b2c85109c39bdef11257470a0c72c6f4603e
Nader Hadji Ghanbari <nader.h.ghanbari@gmail.com>
2015-11-14 23:05:45 +0800

    Improve `pwd` by reading `user.dir` Java system property which points to current working directory.

--- src/pwd.scala
+++ src/pwd.scala
@@ -2,5 +2,10 @@
 
 /** Prints current working directory name. */
 
-def directory = new java.io.File(".").getCanonicalPath
-println(directory)
+val systemPropertyCurrentDir = System getProperty "user.dir"
+lazy val normalCurrentDir = new java.io.File(".").getCanonicalPath
+val directory = Option(systemPropertyCurrentDir) getOrElse normalCurrentDir
+
+println {
+  directory
+}
